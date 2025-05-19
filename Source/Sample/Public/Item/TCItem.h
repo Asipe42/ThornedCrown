b2961583add6ad8a3021e2d0ff5e367fcb9edab9
Modin <eom3658@gmail.com>
2025-05-19 12:52:37 +0900

    아이템 데이터 구조 설계

---
+++ Source/Sample/Public/Item/TCItem.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TCGameplayAbility.h"
+#include "Engine/DataAsset.h"
+#include "TCItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAMPLE_API UTCItem : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+
+	// ============================== Ability ==============================
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Abilities)
+	TSubclassOf<UTCGameplayAbility> GrantedAbility;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Abilities)
+	int32 AbilityLevel;
+};
---
+++ Source/Sample/Public/Item/TCWeaponItem.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2025 Modin. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item/TCItem.h"
+#include "TCWeaponItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAMPLE_API UTCWeaponItem : public UTCItem
+{
+	GENERATED_BODY()
+	
+};
